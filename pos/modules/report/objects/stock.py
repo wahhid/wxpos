e86e82fdf7d0b58e799d46ff3c7e779db7159c47
jadkik94 <jadkik94@gmail.com>
2011-11-05 22:35:35 +0000

    made a common base for pdf reports.

--- pos/modules/report/objects/stock.py
+++ pos/modules/report/objects/stock.py
@@ -2,13 +2,6 @@ import pos
 
 import datetime
 
-import pos.modules.currency.objects.currency as currency
-
-import pos.modules.stock.objects.product as product
-
-def getProducts():
-    return product.find(list=True, in_stock=True)
-
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4, letter
 from reportlab.lib.units import inch
@@ -18,77 +11,50 @@ from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, \
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT
 
-def framePage(canvas,doc):
-    canvas.saveState()
+from pos.modules.report.objects.pdf import PDFReport, stylesheet
 
-    canvas.setFont('Times-Italic',12)
-    canvas.drawRightString(523, doc.bottomMargin+doc.height, "Stock Report")
+import pos.modules.currency.objects.currency as currency
 
-    canvas.setFont('Times-Roman',12)
-    canvas.drawCentredString(doc.leftMargin+doc.width/2, doc.bottomMargin, "Page %d" % doc.page)
-    #canvas.drawString(4 * inch, 0.75 * inch, "Page %d" % doc.page)
-    canvas.restoreState()
+import pos.modules.stock.objects.product as product
+
+class StockPDFReport(PDFReport):
+    
+    def _init_content(self):
+        ps = product.find(list=True, in_stock=True)
+    
+        total = 0
+        defc = currency.default
+        headers = ('Reference', 'Name', 'Price', 'Quantity', 'Total')
+        data = []
+        marked = []
+        for p in ps:
+            reference = p.data['reference']
+            name = p.data['name']
+            price = p.data['price']
+            pc = p.data['currency']
+            quantity = p.data['quantity']
+            
+            data.append([reference,
+                         name,
+                         pc.format(price),
+                         'x%d' % (quantity,),
+                         pc.format(quantity*price)])
+            if quantity<=0:
+                marked.append(len(data))
+
+            total += currency.convert(quantity*price, pc, defc)
+
+        table = self.doTable(data=data, header=headers, marked_rows=marked)
+
+        total_para = Paragraph('Total: %s' % (defc.format(total),),
+                               stylesheet['Heading3Right'])
+        self.elements.append(Spacer(36,36))
+        self.elements.append(total_para)
 
 def generateReport(filename):
-    doc = SimpleDocTemplate(filename)
-    elements = []
+    today = datetime.date.today()
+    rep = StockPDFReport(filename, 'Stock Report',
+                         None,
+                         (today, None))
 
-    stylesheet = getSampleStyleSheet()
-    stylesheet.add(ParagraphStyle(name='Heading3Right',
-                            parent=stylesheet['Heading3'],
-                            alignment=TA_RIGHT),
-                   alias='h3-right')
-    stylesheet.add(ParagraphStyle(name='Subtitle',
-                            parent=stylesheet['Title'],
-                            fontSize=14),
-                   alias='subtitle')
-
-    today = datetime.datetime.today()
-    today = today.strftime('%Y-%m-%d %I:%M%p')
-    elements.append(Paragraph('Stock Report', stylesheet['Title']))
-    elements.append(Paragraph('On: %s' % (today,), stylesheet['Subtitle']))
-
-    ps = getProducts()
-    
-    total = 0
-    defc = currency.default
-    headers = ('Reference', 'Name', 'Price', 'Quantity', 'Total')
-    data = [headers]
-    style = [('LINEBELOW', (0,0), (-1,0), 2, colors.green),
-             ('ALIGN', (0,0), (-1,0), 'CENTER'),
-             ('LINEABOVE', (0,2), (-1,-1), 0.25, colors.black),
-             ('ALIGN', (1,1), (-1,-1), 'RIGHT')]
-    for p in ps:
-        reference = p.data['reference']
-        name = p.data['name']
-        price = p.data['price']
-        pc = p.data['currency']
-        quantity = p.data['quantity']
-
-        if quantity<=0:
-            style.append(('TEXTCOLOR', (0, len(data)), (-1, len(data)), colors.red))
-        
-        data.append([reference,
-                     name,
-                     pc.format(price),
-                     'x%d' % (quantity,),
-                     pc.format(quantity*price)])
-
-        total += currency.convert(quantity*price, pc, defc)
-
-    colwidths = ['17%']+['*']+['17%']*3
-    #colwidths = [float(PAGE_WIDTH)/len(headers)]*len(headers)
-    table = Table(data=data,
-                  colWidths=colwidths,
-                  style=TableStyle(style))
-    elements.append(table)
-
-    elements.append(Spacer(36,36))
-
-    total_para = Paragraph('Stock Total: %s' % (defc.format(total),),
-                           stylesheet['Heading3Right'])
-    elements.append(total_para)
-
-    doc.build(elements, onFirstPage=framePage, onLaterPages=framePage)
-
-    return doc
+    return rep.build()
